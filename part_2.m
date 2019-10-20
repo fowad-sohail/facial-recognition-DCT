f7d694d7fb060af3f54b2c9482ecc222d7e6eaa2
Tim Roche Laptop <kc2vsa@gmail.com>
2019-10-20 15:46:03 -0400

    Added plot of another subject

--- part_2.m
+++ part_2.m
@@ -6,23 +6,34 @@ vector_dim_35 = findfeatures('att_faces/s17/6.pgm', 35);
 vector_dim_100 = findfeatures('att_faces/s17/6.pgm', 100);
 
 figure(1);
-
-subplot(1, 3, 1)
-plot_features(vector_dim_9);
-title('Feature Vector with Dimension 9');
-
-subplot(1, 3, 2)
-plot_features(vector_dim_35);
-title('Feature Vector with Dimension 35');
-
-subplot(1, 3, 3)
-plot_features(vector_dim_100);
-title('Feature Vector with Dimension 100');
-
+plot_person(vector_dim_9, vector_dim_35, vector_dim_100);
 sg = sgtitle('Subject 17 Picture 6 DCT Vector');
 sg.FontSize = 20;
 sg.FontWeight = 'bold';
 
+% ------------------------
+
+vector_dim_9 = findfeatures('att_faces/s18/6.pgm', 9);
+vector_dim_35 = findfeatures('att_faces/s18/6.pgm', 35);
+vector_dim_100 = findfeatures('att_faces/s18/6.pgm', 100);
+
+figure(2);
+plot_person(vector_dim_9, vector_dim_35, vector_dim_100);
+sg = sgtitle('Subject 18 Picture 6 DCT Vector');
+sg.FontSize = 20;
+sg.FontWeight = 'bold';
+
+function plot_person(vector_dim_9, vector_dim_35, vector_dim_100);
+    subplot(1, 3, 1)
+    plot_features(vector_dim_9);
+    title('Feature Vector with Dimension 9');
+    subplot(1, 3, 2)
+    plot_features(vector_dim_35);
+    title('Feature Vector with Dimension 35');
+    subplot(1, 3, 3)
+    plot_features(vector_dim_100);
+    title('Feature Vector with Dimension 100');
+end
 % Given a dct of an image in the zigzag format (given by findfeaturs())
 % Return plot of the feature vector vs its indices
 function plot_features(output_features)
