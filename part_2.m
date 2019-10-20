c869d7ec487d77c2350ccf5380288b7729c51657
sohailf7 <33504467+sohailf7@users.noreply.github.com>
2019-10-20 15:30:32 -0400

    part_2.m Feature Extraction

---
+++ part_2.m
@@ -0,0 +1,32 @@
+clear all;
+%close all;
+
+vector_dim_9 = findfeatures('att_faces/s17/6.pgm', 9);
+vector_dim_35 = findfeatures('att_faces/s17/6.pgm', 35);
+vector_dim_100 = findfeatures('att_faces/s17/6.pgm', 100);
+
+figure(1);
+
+subplot(1, 3, 1)
+plot_features(vector_dim_9);
+title('Feature Vector with Dimension 9');
+
+subplot(1, 3, 2)
+plot_features(vector_dim_35);
+title('Feature Vector with Dimension 35');
+
+subplot(1, 3, 3)
+plot_features(vector_dim_100);
+title('Feature Vector with Dimension 100');
+
+sgtitle('Subject 17 Picture 6 DCT Vector');
+
+% Given a dct of an image in the zigzag format (given by findfeaturs())
+% Return plot of the feature vector vs its indices
+function plot_features(output_features)
+    t = 1:1:length(output_features);
+    y = output_features.'; % transpose column vector to row vector
+    plot(t,y);
+    xlabel('Number of features');
+    ylabel('DCT Output');
+end
