c2af809647366b8f878619c6f6b64b367959fb99
Tim Roche Laptop <kc2vsa@gmail.com>
2019-11-14 21:46:11 -0500

    I works

--- therealPart4.m
+++ therealPart4.m
@@ -6,12 +6,13 @@ ISR = 0;
 kList = 1:2:7;
 dList = 25:15:100;
 
-ISRvector = zeros(length(kVals), length(dctVals));
+%ISRvector = zeros(length(kVals), length(dctVals));
 
-performance = zeros(kVals, dctVals);
 
 dctNum = length(dList);
 kNum = length(kList);
+
+performance = zeros(dctNum, kNum);
 
 for dindex =1:dctNum
     dctlength = dList(dindex);
@@ -19,9 +20,21 @@ for dindex =1:dctNum
     for kindex =1:kNum
         k = kList(kindex);
         ISR = part_4(dctlength, k, trdata_raw, trclass);
-        performance(kindex, dindex) = ISR;
-        
+        performance(dindex, kindex) = ISR*100;
     end
 end
 
-stem3(performance);
+dList = dList';
+dTotal = repmat(dList, 1, kNum);
+kTotal = repmat(kList, dctNum, 1);
+surf(dTotal, kTotal, performance);
+xlabel("DCT Value");
+ylabel("K Value");
+zlabel("ISR");
+ylim([1 7]);
+xlim([25 100]);
+xticks(25:15:100);
+yticks(1:2:7);
+zlim([75 95]);
+zticks(75:5:95);
+title("Success Rate");
